238a965a15b4fd6194e7373bdb0fedc63b255856
Daniel Oaks <daniel@danieloaks.net>
2015-01-26 20:45:12 +1100

    Add CLI build file using cx_Freeze, small fix to let us build binaries, and pip requirements file

---
+++ build_cli.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+from cx_Freeze import setup, Executable
+
+options = {
+    'build_exe': {
+        'packages': [
+            'atexit',
+        ],
+        'excludes': [
+            # 'tkinter',
+        ],
+    },
+    'bdist_mac': {
+        'bundle_name': 'BASC-Archiver',
+        'iconfile': 'build-files/icon.icns',
+        'custom_info_plist': 'build-files/Info.plist',
+    },
+    'bdist_dmg': {
+        'volume_label': 'BASC-Archiver',
+    },
+}
+
+setup(
+    name='BASC-Archiver',
+    version='0.8.2',
+    description='Makes a complete archive of imageboard threads including images, HTML, and JSON.',
+    options=options,
+    executables=[Executable('thread-archiver')],
+)
